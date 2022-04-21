6afb8714074509dc50fd78e3566b4fefa88318cc
cyebukayire <peacecyebukayire@gmail.com>
2022-04-21 11:54:01 +0200

    initial commit

---
+++ index.cpp
@@ -0,0 +1,9 @@
+// #include <sctdlib>
+#include <iostream>
+using namespace std;
+
+int main(){
+    std::cout<<"Hello Peace, Welcome again to my world!";
+    return 0
+    // return EXIT_SUCCESS
+}
