170a520586a08d05918fd9c073a66c26003fe8fb
cyebukayire <peacecyebukayire@gmail.com>
2022-04-26 17:12:38 +0200

    chore: pointers and arrays

---
+++ pointers/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a[5], *p;
+    cout<<*a<<endl<<p<<endl;
+
+    for(int sum = a[0], i=1; i<5; i++){
+        
+    }
+    return 0;
+}
