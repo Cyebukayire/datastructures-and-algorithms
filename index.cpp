eb80eba6e697e5112031b978fb1c7366a9e4b4aa
cyebukayire <peacecyebukayire>
2022-05-10 10:23:39 -0400

    add bases

--- index.cpp
+++ index.cpp
@@ -7,6 +7,10 @@ int main(){
     int b = a++;
     b =8034;
     std::cout<<(b >> 3)<<endl<<a<<endl;
+
+    short x = 0b010101101;
+    short y = 0b00110010;
+    cout<<x+y;
     return 0;
     // return EXIT_SUCCESS
 }
