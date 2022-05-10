62aa0593e8628f4c10b953f62863b89da79ca1a5
cyebukayire <peacecyebukayire>
2022-05-10 10:09:33 -0400

    add bases

--- index.cpp
+++ index.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main(){
-    std::cout<<"Hello Peace, Welcome again to my world!";
+    int a =1;
+    int b = a++;
+    b =8034;
+    std::cout<<(b >> 3)<<endl<<a<<endl;
     return 0;
     // return EXIT_SUCCESS
 }
