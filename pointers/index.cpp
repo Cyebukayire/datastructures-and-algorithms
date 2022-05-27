1739aa0dd5569ba39e74d73ab4a514f3fd556e80
Peace <peacecyebukayire@gmail.com>
2022-05-27 16:37:38 +0200

    Update index.cpp

--- pointers/index.cpp
+++ pointers/index.cpp
@@ -28,11 +28,12 @@ int main(){
     int n = 5, *s = &n, &r = n;
 cout << n << ' ' << *s << ' ' << r << endl;
 
-    // delete p; // this can't delete the pointer
+    // delete p; can't delete the pointer
     /* use delete p; to end program peacefully and to prevent further leakage*/
     
     // p = 0; // atleast this forces p to point to null when we don't want it to point to any variable anymore
     /*Run p=0 at the end of the program since it stops the program from proceeding*/
     // cout<<"After p=0, p: "<<p<<endl<<"*p: "<<*p<<endl<<"i: "<<i<<endl;
+    
     return 0;
 }
