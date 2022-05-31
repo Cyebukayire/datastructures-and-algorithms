8de9ccc018abc86b2c5a78f05da90991c7e3768b
Peace <peacecyebukayire@gmail.com>
2022-05-31 20:19:34 +0200

    Update index.cpp

--- datatypes/index.cpp
+++ datatypes/index.cpp
@@ -14,7 +14,6 @@ int main(){
     const char cars[]= "Ferrari"; //char with string prefers double quoatation
     const char letter[]={'A', 'B', 'C'};//char prefers single quotations
     const char str1[] = "str1";
-    // string letters = ;
     cout<<name<<fname<<endl;
     cout<<cars;
     cout<<endl<<func(23)<<endl;
