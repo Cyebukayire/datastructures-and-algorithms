bbffd157c20688fac61e5271ad7d5e24c35425c0
Peace <peacecyebukayire@gmail.com>
2022-06-07 06:20:03 +0200

    Update index.cpp

--- datatypes/index.cpp
+++ datatypes/index.cpp
@@ -12,9 +12,12 @@ int main(){
     // char name[3] = 'P';
     
     const int num = 23;
-    string fname = "Kaliza"; // Strings preffer double quoatations
-    const char cars[]= "Ferrari"; //char with string prefers double quoatation
-    const char letter[]={'A', 'B', 'C'};//char prefers single quotations
+    string fname = "Kaliza"; 
+    // Strings preffer double quoatations
+    const char cars[]= "Ferrari"; 
+    //char with string prefers double quoatation
+    const char letter[]={'A', 'B', 'C'};
+    //char prefers single quotations
     const char str1[] = "str1";
     cout<<name<<fname<<endl;
     cout<<cars;
