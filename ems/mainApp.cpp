7532fa7153ba7365413bea72c89ec8243647f557
Cyebukayire <peacecyebukayire@gmail.com>
2022-07-02 18:37:02 +0200

    chore: file handling

--- ems/mainApp.cpp
+++ ems/mainApp.cpp
@@ -1,21 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+
 using namespace std;
 
 const int maxrow = 20;
 string empName[maxrow] = {};
 string empID[maxrow] = {};
 
+void openFile()
+{
+    string line;
+    ifstream myfile("./employee.txt");
+    if (myfile.is_open())
+    {
+        int x = 0;
+        while (getline(myfile, line))
+        {
+            int l = line.length();
+            empID[x] = line.substr(0, 3);
+            empName[x] = line.substr(4, l - 4);
+            x++;
+        }
+    }
+}
+void saveFile()
+{
+    ofstream myfile;
+    myfile.open("./employee.txt");
+    for (int x = 0; x < maxrow; x++)
+    {
+        if (empID[x] == "\0")
+        {
+            break;
+        }
+        else
+        {
+            myfile << empID[x] + "," + empName[x] << endl;
+        }
+    }
+}
 void viewEmployees()
 {
     cout << "\n===========";
-    cout << " EMPLOYEES" << endl;
+    cout << " EMPLOYEES ";
     cout << "===========\n";
-    cout << "\nEmpID ---- EmpName\n";
+    cout << "\nEmpID \t\t|\t\t EmpName\n";
+    cout << "\t_______________________\n\n";
     for (int i = 0; i < maxrow; i++)
     {
-        cout << empName[i] << "-------" << empID[i] << endl;
+        if (empID[i] == "\0")
+        {
+            break;
+        }
+        cout << empID[i] << "\t\t\t\t" << empName[i] << endl;
     }
 }
 
@@ -23,7 +64,6 @@ void createEmployee()
 {
     string name;
     string id;
-
     cin.ignore();
     cout << "\nEnter your ID: ";
     getline(cin, id);
@@ -38,14 +78,93 @@ void createEmployee()
         {
             empID[i] = id;
             empName[i] = name;
-            cout << "Employee is created successfully.";
+            cout << "Employee is created successfully." << endl;
             break;
         }
     }
 }
 
+void searchEmployee()
+{
+    string id;
+    bool found = false;
+    cin.ignore();
+    cout << "\nEnter the employee ID: ";
+    getline(cin, id);
+
+    for (int i = 0; i < maxrow; i++)
+    {
+        if (empID[i] == id)
+        {
+            found = true;
+            cout << "\nEmpID \t\t|\t\t EmpName\n";
+            cout << "\t_______________________\n\n";
+            cout << empID[i] << "\t\t\t\t" << empName[i] << endl;
+            break;
+        }
+    }
+    if (!found)
+    {
+        cout << "\nEmployee Not Found!\n\n";
+    }
+}
+void deleteEmployee()
+{
+    string id;
+    bool found = false;
+    cin.ignore();
+    cout << "\nEnter the employee ID: ";
+    getline(cin, id);
+
+    for (int i = 0; i < maxrow; i++)
+    {
+        if (empID[i] == id)
+        {
+            empID[i] = "";
+            empName[i] = "";
+            cout << "\nDeleted employee succefully!\n\n";
+            break;
+        }
+    }
+    if (!found)
+    {
+        cout << "\nEmployee Not Found!\n\n";
+    }
+}
+
+void updateEmployee()
+{
+    string id, newID, newName;
+    bool found = false;
+    cin.ignore();
+    cout << "\nWhat's the Employee ID? ";
+    getline(cin, id);
+    // Search
+    for (int i = 0; i < maxrow; i++)
+    {
+        if (empID[i] == id)
+        {
+            found = true;
+            cout << "\nEnter new employee ID: ";
+            getline(cin, newID);
+            cout << "\nEnter new employee Name: ";
+            getline(cin, newName);
+            empName[i] = newName;
+            empID[i] = newID;
+            cout << "\n\nEmployee Updated Successfully\n\n";
+            viewEmployees();
+            break;
+        }
+    }
+    if (!found)
+    {
+        cout << "\nEmployee Not Found!\n\n";
+    }
+}
+
 int main()
 {
+    openFile();
     int choice;
     do
     {
@@ -54,10 +173,11 @@ int main()
         cout << "1. View employees\n"
              << "2. Add new employee\n";
         cout << "3. Update an employee\n"
-             << "4. Delete an employee\n";
-        cout << "5. Quit" << endl
+             << "4. Delete an employee\n"
+             << "5. Search for an employee\n";
+        cout << "6. Quit"
              << endl;
-        cout << "Choose: ";
+        cout << "\nChoose: ";
         cin >> choice;
         cout << endl;
         switch (choice)
@@ -69,13 +189,16 @@ int main()
             createEmployee();
             break;
         case 3:
-            cout << "Update employee" << endl;
+            updateEmployee();
             break;
         case 4:
-            cout << "Delete an Employee" << endl;
+            deleteEmployee();
             break;
         case 5:
-            cout << "QUITING..." << endl;
+            searchEmployee();
+            break;
+        case 6:
+            cout << "Exiting ..." << endl;
             goto end;
             break;
         default:
@@ -85,5 +208,6 @@ int main()
     } while (choice != 6);
     cout << endl;
 end:;
+    saveFile();
     return 0;
 }
